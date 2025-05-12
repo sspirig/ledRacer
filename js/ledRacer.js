aa155b171aa27480b6395540fe9b03123e33fd45
sspirig <santiago.sprg@eduge.ch>
2025-05-12 16:09:02 +0200

    Finished ledRacer

--- js/ledRacer.js
+++ js/ledRacer.js
@@ -1,5 +1,10 @@
 "use strict";
-
+/**
+ * Projet: ledRacer
+ * Fichier: fichier javascript
+ * Auteur: Santiago Spirig
+ * Date: 12.05.2025
+ */
 
 // variable globale pour l'envoi et la réception de messages vers l'équipement bluetooth distant
 var rx = null;
@@ -10,24 +15,40 @@ const serviceUuid = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 let device = null;
 let countRed = 0;
 let countBlue = 0;
-const testGameDiv = document.querySelector(".testGame");
+const body = document.querySelector("body");
+const GameButtonsDiv = document.querySelector(".gameButtons");
 const blueBtn = document.querySelector("#blueBtn");
 const redBtn = document.querySelector("#redBtn");
-const customCmdBtn = document.querySelector("#custom");
 const toggleCmdBtn = document.querySelector("#toggle");
 const initBtn = document.querySelector("#initBtn");
-const listBtn = document.querySelector("#listBtn");
 const connectBtn = document.querySelector("#connectBtn");
 const info = document.querySelector("#info");
+const raceBox = document.querySelector(".raceBox");
+let timerIntervalId = "";
+let isRaceFinished = false;
+let sec;
+let redName = "";
+let blueName = "";
 initBtn.addEventListener("click", e => {init();});
-listBtn.addEventListener("click", e => {listServices();});
 connectBtn.addEventListener("click", e => {connect();});
-customCmdBtn.addEventListener("click", e => {
-    let value = document.querySelector('#myCmdInput').value;
-    sendCmd(value);
-});
 toggleCmdBtn.addEventListener("click", e => {
+    console.log("displayed as", GameButtonsDiv.style.display);
+    if (GameButtonsDiv.style.display == "none") {
+        GameButtonsDiv.style.display = "flex";
+
+
+
+        raceBox.querySelector("#red").innerHTML = "";
+        raceBox.querySelector("#blue").innerHTML = "";
+        info.innerHTML = "";
+        body.style.backgroundColor = "rgb(255, 255, 255)";
+        redBtn.addEventListener("touchstart", sendMoveCmd);
+        blueBtn.addEventListener("touchstart", sendMoveCmd);
+        StartRace();
+    }
     sendCmd(e.target.id);
+    ShowStartRaceBtnToggle();
+    console.log("displayed now as", GameButtonsDiv.style.display);
 });
 
 const redRoad = document.querySelector(".road#red");
@@ -85,30 +106,42 @@ async function connect() {
                 break;
             case "count":
                 countRed += 1;
-                info.innerHTML = countRed;
+                //info.innerHTML = countRed;
                 break;
             default:
                 break;
         }
-        document.getElementById("status").innerHTML = message.split(":")[1];
+        // document.getElementById("status").innerHTML = message.split(":")[1];
     });
 
-
+    redName = prompt("Insérer le nom du joueur rouge :", "red");
+    blueName = prompt("Insérer le nom du joueur bleu :", "blue");
+    if (redName == "") redName = "red";
+    if (blueName == "") blueName = "blue";
+    initBtn.style.display = "none";
+    connectBtn.style.display = "none";
     document.getElementById("status").innerHTML = "Connexion RX/TX effectuée";
-    EnableTestGame();
+    sendCmd("toggle");
+    ShowStartRaceBtnToggle();
 }
 
 async function sendCmd(cmd) {
     const encoder = new TextEncoder();
-    document.getElementById("status").innerHTML = `commande à envoyer: ${cmd}`;
+    // document.getElementById("status").innerHTML = `commande à envoyer: ${cmd}`;
     const cmdBytes = encoder.encode(cmd);
     await tx.writeValue(cmdBytes);
 }
 
-function EnableTestGame() {
-    testGameDiv.style.display = "flex";
-    redBtn.addEventListener("touchstart", sendMoveCmd);
-    blueBtn.addEventListener("touchstart", sendMoveCmd);
+function StartRace() {
+
+    sec = 0;
+    isRaceFinished = false;
+    timerIntervalId = setInterval( ms => {
+
+            sec += 1;
+            document.getElementById("status").innerHTML = sec+"s";
+    }, 1000);
+    
 }
 
 function sendMoveCmd(event) {
@@ -124,21 +157,62 @@ function sendMoveCmd(event) {
     }
 }
 
+function ShowStartRaceBtnToggle() {
+    if (toggleCmdBtn.style.display == "none"){
+        toggleCmdBtn.style.display = "flex";
+    }
+    else {
+        toggleCmdBtn.style.display = "none";
+    }
+    
+    
+}
+
+function RegisterTime(name, sec) {
+    if (localStorage.getItem("ledRacer_scoreboard")[name] != undefined) {
+        if (parseInt(localStorage.getItem("ledRacer_scoreboard")[name]) < sec) {
+
+            let scoreboard = localStorage.getItem("ledRacer_scoreboard");
+            scoreboard[name] = sec;
+            localStorage.setItem("ledRacer_scoreboard", scoreboard);
+        }
+    }
+    else {
+        let scoreboard = localStorage.getItem("ledRacer_scoreboard");
+        scoreboard[name] = sec;
+        localStorage.setItem("ledRacer_scoreboard", scoreboard);
+    }
+}
+
 function Move(which, pos) {
 
     if (which == "red") {
         redRoad.innerHTML = redRoad.innerHTML+'<div id="red" class="roadLed"></div>';
         if (parseInt(pos) == 120) {
+            GameButtonsDiv.style.display = "none";
             info.style.color = "crimson";
-            info.innerHTML = "Le joueur rouge a gagné";
+            body.style.backgroundColor = "rgb(255, 174, 191)";
+            info.innerHTML = "Le joueur rouge ("+redName+") a gagné en "+sec+" secondes";
+            RegisterTime(redName, sec);
+
+            sendCmd("finishRace red");
+            clearInterval(timerIntervalId);
+            ShowStartRaceBtnToggle();
             return;
         }
     }
     else {
         blueRoad.innerHTML = blueRoad.innerHTML+'<div id="blue" class="roadLed"></div>';
         if (parseInt(pos) == 120) {
+            GameButtonsDiv.style.display = "none";
             info.style.color = "dodgerblue";
-            info.innerHTML = "Le joueur bleu a gagné";
+            body.style.backgroundColor = "rgb(170, 213, 255)";
+            info.innerHTML = "Le joueur bleu ("+blueName+") a gagné en "+sec+" secondes";
+            RegisterTime(blueName, sec);
+
+            sendCmd("finishRace blue");
+            clearInterval(timerIntervalId);
+            ShowStartRaceBtnToggle();
             return;
         }
     }
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Lien vers le github :
+https://github.com/sspirig/ledRacer
